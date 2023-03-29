671d02e17c73413be5ff523d355443874d2e1a83
ByBeta1 <100860273+ByBeta1@users.noreply.github.com>
2023-03-29 16:28:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+/*let a = 5;
+let b = 3;
+console.log( `la suma de a + b = ${a+b} `);
+
+
+var manolo = {NOMBRE: "alfredo", EDAD:"21", APELlIDO:"sanchez"} 
+
+var mayorEdad = `${manolo.NOMBRE} es ${manolo.EDAD>=21 ? 'MAYOR DE EDAD' : 'MENOR DE EDAD'}`
+
+
+console.log(mayorEdad)
+
+
+
+const menorDetector = 17
+
+if(menorDetector <18)
+{
+    print ("usted no es mayor")
+
+}
+else{
+    print("usted es mayor")
+}
+
+console.log(menorDetector)
+
+function miFuncion (nombre,apellido){
+    console.log(nombre)
+}
+miFuncion("alfredo")
+
+const miFuncion2 = function(nombre,apellido){
+    console.log(nombre)
+}
+
+miFuncion("alfredo2")
+
+var nums = [1,2,3,4,5]
+nums.map(function(num){
+
+    console.log(num)
+})*/
